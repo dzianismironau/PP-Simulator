bc3921430e424abe6ad2721871d07aa3df602d73
dzianismironau <denis.mironow2@gmail.com>
2024-10-29 19:17:04 +0100

    Settery

--- Simulator/Animals.cs
+++ Simulator/Animals.cs
@@ -2,9 +2,28 @@
 
     public class Animals
     {
-    public required string Description { get; init; }
+    private string _description = "Unknown";
+    public string Description 
+    { 
+        get=> _description;
+        set
+        {
+            _description = value?.Trim() ?? "Unknown";
+            if (_description.Length < 3) 
+                _description = _description.PadRight(3, '#');
+
+            if (_description.Length > 15)
+            {
+                _description = _description.Substring(0, 15).TrimEnd();
+                if (_description.Length < 3)
+                    _description = _description.PadRight(3, '#');
+            }
+            if (char.IsLower(_description[0]))
+                _description = char.ToUpper(_description[0]) + _description.Substring(1);
+        }
+    }
 
     public uint Size { get; set; } = 3;
 
-    public string Info => $"{Description} <Size>"; 
+    public string Info => $"{Description} <{Size}>"; 
     }
--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -1,8 +1,45 @@
-﻿namespace Simulator;
+﻿using System.Reflection.Emit;
+using System.Xml.Linq;
+
+namespace Simulator;
 public class Creature
     {
-    public string? Name { get; set; }
-    public int Level { get; set; }
+    private string _name = "Unknown";
+    private int _level = 1;
+    public string? Name 
+    { 
+        get => _name; 
+        set 
+        {
+            if (_name != "Unknown") return;
+            _name = value?.Trim() ?? "Unknown";
+
+            if (_name.Length < 3) 
+                _name = _name.PadRight(3, '#');
+
+            if (_name.Length > 25)
+            {
+                _name = _name.Substring(0, 25).TrimEnd();
+
+                if (_name.Length < 3)
+                    _name = _name.PadRight(3, '#');
+            }
+            if (char.IsLower(_name[0])) 
+                _name = char.ToUpper(_name[0]) + _name.Substring(1);
+        }
+    }
+    public int Level 
+    {
+        get => _level;
+        set
+        {
+            if (value < 1)
+                _level = 1;
+            else if (value >= 10)
+                _level = 10;
+            else _level = value;
+        }
+    }
 
     public Creature(string name, int level = 1)
     { Name = name; Level = level; }
@@ -16,5 +53,10 @@ public class Creature
     }
 
     public string Info => $"{Name} [{Level}]";
-    }
 
+    public void Upgrade()
+    {
+        if (Level < 10) 
+            Level++;
+    }
+}
